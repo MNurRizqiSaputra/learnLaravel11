fb7ee68647803e7fca7ebf376b9220295096c50e
MNurRizqiSaputra <putra15x@gmail.com>
2025-01-27 14:18:44 +0700

    First Commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Contact</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/blog">Blog</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+    </nav>
+    <h1>Halaman Contact</h1>
+    <p>Hubungi saya melalui media sosial di bawah ini:</p>
+
+    <!-- Informasi Sosial Media -->
+    <div class="social-media">
+        <a href="https://www.instagram.com/yourusername" target="_blank" class="social-icon instagram">Instagram</a>
+        <a href="https://www.facebook.com/yourusername" target="_blank" class="social-icon facebook">Facebook</a>
+        <a href="https://twitter.com/yourusername" target="_blank" class="social-icon twitter">Twitter</a>
+        <a href="https://www.linkedin.com/in/yourusername" target="_blank" class="social-icon linkedin">LinkedIn</a>
+    </div>
+
+</body>
+</html>
